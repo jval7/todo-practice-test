76d3307ce85f6f906679f3dc0c5c0f6b9c39b700
Jhon Valderrama <jhonvalderramaa7@gmail.com>
2024-11-05 17:44:27 -0500

    Add CI

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -11,8 +11,14 @@ class RAGServiceSingleton:
     def get_instance(cls) -> usecases.RAGService:
         if cls._instance is None:
             configs = configurations.Configs()
-            openai_adapter = OpenAIAdapter(api_key=configs.openai_api_key, model=configs.model,
-                                           max_tokens=configs.max_tokens, temperature=configs.temperature)
+            openai_adapter = OpenAIAdapter(
+                api_key=configs.openai_api_key,
+                model=configs.model,
+                max_tokens=configs.max_tokens,
+                temperature=configs.temperature,
+            )
             document_repo = ChromaDBAdapter()
-            cls._instance = usecases.RAGService(document_repo=document_repo, openai_adapter=openai_adapter)
+            cls._instance = usecases.RAGService(
+                document_repo=document_repo, openai_adapter=openai_adapter
+            )
         return cls._instance
